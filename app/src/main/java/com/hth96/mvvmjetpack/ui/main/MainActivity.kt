1a5d147b5227c157ea8dd86548a3bd6d69900402
Hactieuho96 <Hactieuho96@gmail.com>
2021-09-13 16:48:02 +0700

    - Adding implementations

--- app/src/main/java/com/hth96/mvvmjetpack/ui/main/MainActivity.kt
+++ app/src/main/java/com/hth96/mvvmjetpack/ui/main/MainActivity.kt
@@ -1,13 +1,16 @@
-package com.hth96.mvvmjetpack.ui
+package com.hth96.mvvmjetpack.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import com.hth96.mvvmjetpack.R
 import com.hth96.mvvmjetpack.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    private val viewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
---
+++ app/src/main/java/com/hth96/mvvmjetpack/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.hth96.mvvmjetpack.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+}
