2fa41560a63461079c5c4c036f5f4023074fd918
Hactieuho96 <Hactieuho96@gmail.com>
2021-09-14 09:42:17 +0700

    - Setup user item

--- app/src/main/java/com/hth96/mvvmjetpack/util/Util.kt
+++ app/src/main/java/com/hth96/mvvmjetpack/util/Util.kt
@@ -2,56 +2,19 @@ package com.hth96.mvvmjetpack.util
 
 import android.content.SharedPreferences
 import android.widget.Button
+import androidx.appcompat.widget.AppCompatImageView
 import androidx.core.content.edit
 import androidx.databinding.BindingAdapter
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.MutableLiveData
 import com.blankj.utilcode.util.ToastUtils
+import com.bumptech.glide.Glide
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import com.hth96.mvvmjetpack.R
 import com.hth96.mvvmjetpack.resource.Resource
 import java.text.SimpleDateFormat
 import java.util.*
-
-inline fun <reified T> SharedPreferences.saveItem(key: String, item: T?) {
-    val json = Gson().toJson(item)
-    edit { putString(key, json) }
-}
-
-inline fun <reified T> SharedPreferences.getItem(key: String): T? {
-    val json = getString(key, "")
-    if (!json.isNullOrBlank()) {
-        val type = object : TypeToken<T>() {}.type
-        return Gson().fromJson(json, type)
-    }
-    return null
-}
-
-inline fun <reified T> SharedPreferences.getList(spListKey: String): ArrayList<T>? {
-    val listJson = getString(spListKey, "")
-    if (!listJson.isNullOrBlank()) {
-        val type = object : TypeToken<ArrayList<T>>() {}.type
-        return Gson().fromJson(listJson, type)
-    }
-    return null
-}
-
-fun Date.formatDate() : String? {
-    val dateTimeFormat = SimpleDateFormat("EEEE, dd MMMM", Locale.US)
-    dateTimeFormat.timeZone = TimeZone.getTimeZone("UTC")
-    return dateTimeFormat.format(this)
-}
-
-fun Date.formatTime() : String? {
-    val dateTimeFormat = SimpleDateFormat("HH:mm:ss", Locale.US)
-    dateTimeFormat.timeZone = TimeZone.getTimeZone("UTC")
-    return dateTimeFormat.format(this)
-}
-
-@BindingAdapter("app:onClick", "app:key")
-fun Button.onClick(check: (String?) -> Unit, key: String?) {
-    setOnClickListener { check(key) }
-}
 
 fun<T: Any?> handleError(
     data: MutableLiveData<Resource<T>>,
@@ -60,10 +23,10 @@ fun<T: Any?> handleError(
     data.postValue(Resource.Error(errorText, data.value?.data))
 }
 
-fun<T: Any?> LifecycleOwner.handleError(result: MutableLiveData<Resource<T>>) {
-    result.observe(this) {
-        if (it is Resource.Error) {
-            ToastUtils.showShort(it.message)
-        }
-    }
+@BindingAdapter("app:userAvatar")
+fun AppCompatImageView.userAvatar(url: String?) {
+    Glide.with(context)
+        .load(url)
+        .placeholder(R.drawable.loading)
+        .into(this)
 }
