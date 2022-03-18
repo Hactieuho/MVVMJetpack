34af3b2efbbe94c82df1c60d1d07612319b0c87b
Hactieuho <Hactieuho96@gmail.com>
2022-03-18 08:28:56 +0700

    Filter user

--- app/src/main/java/com/hth96/mvvmjetpack/ui/main/MainViewModel.kt
+++ app/src/main/java/com/hth96/mvvmjetpack/ui/main/MainViewModel.kt
@@ -1,5 +1,7 @@
 package com.hth96.mvvmjetpack.ui.main
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.hth96.mvvmjetpack.data.UserRepository
@@ -11,6 +13,16 @@ import javax.inject.Inject
 class MainViewModel @Inject constructor(
     val userRepository: UserRepository
 ) : ViewModel() {
+
+    val filterText = MutableLiveData("");
+    val filteredUserList = Transformations.switchMap(userRepository.getUsersResult) { l ->
+        Transformations.map(filterText) { f ->
+            l.data?.filter { u ->
+                u?.email?.contains(f, true) == true || u?.fullName()?.contains(f, true) == true
+            }
+        }
+    }
+
     fun fetchUsers(currentPage: Int? = 1) = viewModelScope.launch {
         userRepository.fetchUsers(currentPage)
     }
