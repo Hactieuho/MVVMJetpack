4184406dc24d8e9eeef4a15d436eedb44a47f6c6
Hactieuho <Hactieuho96@gmail.com>
2022-03-18 09:23:00 +0700

    Filter user

--- app/src/main/java/com/hth96/mvvmjetpack/ui/adapter/viewholder/DataBindingViewHolder.kt
+++
@@ -1,8 +0,0 @@
-package com.hth96.mvvmjetpack.ui.adapter.viewholder
-
-import androidx.databinding.ViewDataBinding
-import androidx.recyclerview.widget.RecyclerView
-
-class DataBindingViewHolder(
-    val binding: ViewDataBinding
-) : RecyclerView.ViewHolder(binding.root)
